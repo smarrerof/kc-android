bda9abb13fb13f49b5d6cf8696661e2f85e0ec50
Sergio Marrero <smarrerof@gmail.com>
2017-11-20 11:35:22 +0000

    Removed dish list fragment and moved all that logic to dish list activity

--- app/src/main/java/com/sergiomarrero/dishr/activity/DishListActivity.kt
+++ app/src/main/java/com/sergiomarrero/dishr/activity/DishListActivity.kt
@@ -1,7 +1,6 @@
 package com.sergiomarrero.dishr.activity
 
 import android.app.Activity
-import android.app.Instrumentation
 import android.content.Context
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
@@ -10,15 +9,10 @@ import android.support.v7.widget.DefaultItemAnimator
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.view.View
-import android.widget.ArrayAdapter
-import android.widget.ListView
 import com.sergiomarrero.dishr.R
 import com.sergiomarrero.dishr.adapter.DishRecyclerViewAdapter
-import com.sergiomarrero.dishr.fragment.DishListFragment
 import com.sergiomarrero.dishr.model.Dish
 import com.sergiomarrero.dishr.model.Dishes
-import com.sergiomarrero.dishr.model.Table
-import com.sergiomarrero.dishr.model.Tables
 import com.sergiomarrero.dishr.repository.DishRepository
 import kotlinx.coroutines.experimental.Deferred
 import kotlinx.coroutines.experimental.android.UI
@@ -26,7 +20,7 @@ import kotlinx.coroutines.experimental.async
 import org.jetbrains.anko.coroutines.experimental.bg
 
 
-class DishListActivity : AppCompatActivity(), DishListFragment.OnDishSelectedListener {
+class DishListActivity : AppCompatActivity() {
 
     companion object {
         val EXTRA_DISH = "EXTRA_DISH"
@@ -36,6 +30,9 @@ class DishListActivity : AppCompatActivity(), DishListFragment.OnDishSelectedLis
             return intent
         }
     }
+
+    lateinit var dishes: List<Dish>
+    lateinit var recyclerView: RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -49,27 +46,43 @@ class DishListActivity : AppCompatActivity(), DishListFragment.OnDishSelectedLis
 
             task.await()
 
-            if (fragmentManager.findFragmentById(R.id.fragment_dish_list) == null) {
-                val fragment = DishListFragment.newInstance(Dishes.toList())
-                fragmentManager.beginTransaction()
-                        .add(R.id.fragment_dish_list, fragment)
-                        .commit()
-            }
+            // Get dishes locally
+            dishes = Dishes.toList()
+
+            // Configure recyclerView
+            recyclerView = findViewById<RecyclerView>(R.id.dish_list)
+            recyclerView.layoutManager = LinearLayoutManager(this@DishListActivity)
+            recyclerView.itemAnimator = DefaultItemAnimator()
+            setAdapter()
         }
     }
 
-    override fun onDishSelected(dish: Dish?) {
-        val intent = Intent()
-        intent.putExtra(EXTRA_DISH, dish)
-
-        setResult(Activity.RESULT_OK, intent)
-
-        finish()
-    }
 
     private fun downloadDishes() {
         val repository = DishRepository()
         val dishes = repository.get()
         Dishes.add(dishes)
+    }
+
+
+    private fun setAdapter() {
+        // Set the adapter
+        val adapter = DishRecyclerViewAdapter(dishes)
+        recyclerView.adapter = adapter
+
+        // Handle click
+        adapter.onClickListener = View.OnClickListener { v: View? ->
+            // Get selected dish
+            val position = recyclerView.getChildAdapterPosition(v)
+            val dish = dishes[position]
+
+            // Return selected dish
+            val intent = Intent()
+            intent.putExtra(EXTRA_DISH, dish)
+
+            setResult(Activity.RESULT_OK, intent)
+
+            finish()
+        }
     }
 }
