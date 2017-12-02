a7911a5aaef85f00a3592db500be89bdde4b75ee
Sergio Marrero <smarrerof@gmail.com>
2017-12-02 08:01:04 +0000

    Fixed typo problem that crashed the app

--- app/src/main/java/com/sergiomarrero/dishr/activity/SplashScreenActivity.kt
+++ app/src/main/java/com/sergiomarrero/dishr/activity/SplashScreenActivity.kt
@@ -37,14 +37,14 @@ class SplashScreenActivity : AppCompatActivity() {
     private fun downloadTables() {
         val repository = TableRepository()
         val tables = repository.get()
-        tables.clear()
+        Tables.clear()
         Tables.add(tables)
     }
 
     private fun downloadDishes() {
         val repository = DishRepository()
         val dishes = repository.get()
-        dishes.clear()
+        Dishes.clear()
         Dishes.add(dishes)
     }
 }
